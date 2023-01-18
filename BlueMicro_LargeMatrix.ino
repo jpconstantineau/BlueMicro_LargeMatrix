24469a94086ae061552e391eb5b6fef139d9d699
Pierre Constantineau <jpconstantineau@gmail.com>
2023-01-17 21:50:50 -0600

    updated for proper HID reports

--- BlueMicro_LargeMatrix.ino
+++ BlueMicro_LargeMatrix.ino
@@ -2,8 +2,12 @@
 // Uses Adafruit Feather nRF52832
 // needs the following libraries
 // - bluemicro_hid
+#include <cstdint>
+#include <vector>
 #include <bluemicro_hid.h>
 #include "hid_keycodes.h"
+
+
 
 #define MATRIX_ROWS 4
 #define MATRIX_COLS 12
@@ -23,11 +27,16 @@ uint8_t keymap[] =    {KC_ESC,    KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC
               KC_LSFT,   KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,    KC_N,    KC_M,  KC_COMMA,KC_DOT,  KC_SLASH, KC_ENTER, \
               KC_LCTL,   KC_LGUI, KC_LALT, KC_RGUI,LAYER_1, KC_SPC,  KC_SPC, LAYER_2,KC_LEFT, KC_UP,   KC_DOWN,  KC_RIGHT};
 
+std::vector<uint16_t> activeKeys;
+std::vector<uint8_t> reportvector;
+
 void setup() {
   // put your setup code here, to run once:
   bluemicro_hid.begin(); 
   Serial.begin(115200);
   Serial.println("BlueMicro_HID Large Matrix Tests");
+  activeKeys.reserve(10);
+  reportvector.reserve(7);
 }
 
 
@@ -36,11 +45,9 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
     // use to send key release report
-    static bool has_key = false;
+    static bool has_key = false;  // must be static to remember previous state
     uint32_t pinreg = 0;
-    bool btn_pressed = false;
 
- 
     //Setting up Scanning, Enabling all columns
     for (int i = 0; i < MATRIX_COLS; ++i){
         pinMode(columns[i], INPUT_PULLUP);
@@ -62,10 +69,8 @@ void loop() {
         int ulPin = g_ADigitalPinMap[columns[i]]; 
         if((pinreg>>ulPin)&1)  
           {
-            btn_pressed = true;
-            uint8_t keycode[6] = { 0 };
-            keycode[0] = keymap[0*MATRIX_COLS + i];
-            bluemicro_hid.keyboardReport(0, keycode);
+            uint8_t keycode = keymap[0*MATRIX_COLS + i];
+            activeKeys.push_back(keycode);
             Serial.print("row: 0 Column");
             Serial.println(i);
             has_key = true;
@@ -82,10 +87,8 @@ void loop() {
         int ulPin = g_ADigitalPinMap[columns[i]]; 
         if((pinreg>>ulPin)&1)  
           {
-            btn_pressed = true;
-            uint8_t keycode[6] = { 0 };
-            keycode[0] = keymap[1*MATRIX_COLS + i];
-            bluemicro_hid.keyboardReport(0, keycode);
+            uint8_t keycode = keymap[1*MATRIX_COLS + i];
+            activeKeys.push_back(keycode);
             Serial.print("row: 1 Column");
             Serial.println(i);
             has_key = true;
@@ -102,10 +105,8 @@ void loop() {
         int ulPin = g_ADigitalPinMap[columns[i]]; 
         if((pinreg>>ulPin)&1)  
           {
-            btn_pressed = true;
-            uint8_t keycode[6] = { 0 };
-            keycode[0] = keymap[2*MATRIX_COLS + i];
-            bluemicro_hid.keyboardReport(0, keycode); 
+            uint8_t keycode = keymap[2*MATRIX_COLS + i];
+            activeKeys.push_back(keycode); 
             Serial.print("row: 2 Column");
             Serial.println(i);
             has_key = true;
@@ -119,10 +120,8 @@ void loop() {
         int ulPin = g_ADigitalPinMap[columns[i]]; 
         if((pinreg>>ulPin)&1)  
           {
-            btn_pressed = true;
-            uint8_t keycode[6] = { 0 };
-            keycode[0] = keymap[3*MATRIX_COLS + i];
-            bluemicro_hid.keyboardReport(0, keycode);
+            uint8_t keycode = keymap[3*MATRIX_COLS + i];
+            activeKeys.push_back(keycode);
             Serial.print("row: 3 Column");
             Serial.println(i);
             has_key = true;
@@ -133,13 +132,69 @@ void loop() {
     for (int i = 0; i < MATRIX_COLS; ++i) {                             
         pinMode(columns[i], INPUT);                                     
     }
-    if ( ! btn_pressed )
+    if ( activeKeys.empty() ) //&& has_key
     {
       // send empty key report if previously has key pressed
-      if (has_key) bluemicro_hid.keyboardRelease();
+      if (has_key) 
+      { 
+        bluemicro_hid.keyboardRelease();
+        Serial.println("Release"); 
+      }
       has_key = false;
+      
+    } 
+    else
+    {
+        uint8_t currentMod = 0;
+        for (auto keycode : activeKeys) 
+        {
+            auto hidKeycode = static_cast<uint8_t>(keycode & 0x00FF);
+            auto extraModifiers = static_cast<uint8_t>((keycode & 0xFF00) >> 8);
+    
+            if (hidKeycode >= KC_A && hidKeycode <= KC_EXSEL)
+            {
+              // add hidKeycode to report vector
+              reportvector.push_back(hidKeycode);
+            }
+            
+            //check if the hid keycode contains a modifier. // also check for macros.
+            switch (hidKeycode) { 
+                case KC_LCTRL:  currentMod |= 1;    currentMod |= extraModifiers; break;
+                case KC_LSHIFT: currentMod |= 2;    currentMod |= extraModifiers; break;
+                case KC_LALT:   currentMod |= 4;    currentMod |= extraModifiers; break;
+                case KC_LGUI:   currentMod |= 8;    currentMod |= extraModifiers; break;
+                case KC_RCTRL:  currentMod |= 16;   currentMod |= extraModifiers; break;
+                case KC_RSHIFT: currentMod |= 32;   currentMod |= extraModifiers; break;
+                case KC_RALT:   currentMod |= 64;   currentMod |= extraModifiers; break;
+                case KC_RGUI:   currentMod |= 128;  currentMod |= extraModifiers; break;
+            }
+            //add all of the extra modifiers into the curren modifier 
+            currentMod |= extraModifiers;
+    
+        }
+
+            uint8_t keycode[6] = { 0 };
+            uint8_t keycodeposition = 0;
+            for (auto thiskeycode : activeKeys) 
+            {
+              if (keycodeposition<6)
+              {
+                  keycode[keycodeposition] = thiskeycode;
+              }
+              keycodeposition++;
+            }
+            
+      
+            bluemicro_hid.keyboardReport(currentMod, keycode);
+            activeKeys.clear();
+            reportvector.clear();
+            Serial.println("Send");
     }
+
+    
   bluemicro_hid.processQueues(CONNECTION_MODE_AUTO);
+
+
     // poll gpio once each 10 ms
   delay(10);
 }
