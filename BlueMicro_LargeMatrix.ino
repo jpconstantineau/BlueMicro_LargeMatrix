8463d6745aa18c8921b88eaef7f61c0aeafee7e3
Pierre Constantineau <jpconstantineau@gmail.com>
2023-01-19 23:06:43 -0600

    added loop and moved to functions

--- BlueMicro_LargeMatrix.ino
+++ BlueMicro_LargeMatrix.ino
@@ -22,12 +22,14 @@
 byte rows[] MATRIX_ROW_PINS;        // Contains the GPIO Pin Numbers 
 byte columns[] MATRIX_COL_PINS;     // Contains the GPIO Pin Numbers  
 
-uint8_t keymap[] =    {KC_ESC,    KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,     KC_BSPACE, \
+uint16_t keymap[] =    {KC_ESC,    KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,     KC_BSPACE, \
               KC_TAB,    KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN,  KC_QUOT, \
               KC_LSFT,   KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,    KC_N,    KC_M,  KC_COMMA,KC_DOT,  KC_SLASH, KC_ENTER, \
               KC_LCTL,   KC_LGUI, KC_LALT, KC_RGUI,LAYER_1, KC_SPC,  KC_SPC, LAYER_2,KC_LEFT, KC_UP,   KC_DOWN,  KC_RIGHT};
 
-std::vector<uint16_t> activeKeys;
+typedef std::vector <uint16_t> trigger_keycodes_t;
+
+trigger_keycodes_t activeKeys;
 std::vector<uint8_t> reportvector;
 
 void setup() {
@@ -40,12 +42,9 @@ void setup() {
 }
 
 
-
-
-void loop() {
-  // put your main code here, to run repeatedly:
-    // use to send key release report
-    static bool has_key = false;  // must be static to remember previous state
+trigger_keycodes_t scanMatrix(trigger_keycodes_t activeKeys)
+{
+    bool has_key = false;
     uint32_t pinreg = 0;
 
     //Setting up Scanning, Enabling all columns
@@ -53,98 +52,68 @@ void loop() {
         pinMode(columns[i], INPUT_PULLUP);
     } 
 
-
-// FIRST ROW
+    // FIRST ROW
     pinMode(rows[0], OUTPUT);
     digitalWrite(rows[0],LOW);
     nrfx_coredep_delay_us(1);   // need for the GPIO lines to settle down electrically before reading first row;
     // READ FIRST ROW
     pinreg = (NRF_GPIO->IN)^0xffffffff;  // press is active high regardless of diode dir
     pinMode(rows[0], INPUT);                                        // 'disables' the row that was just scanned  
-// SECOND ROW
-    pinMode(rows[1], OUTPUT);
-    digitalWrite(rows[1],LOW);
-    // PROCESS FIRST ROW - need for the GPIO lines to settle down electrically before reading second row;
-    for (int i = 0; i < MATRIX_COLS; ++i){
-        int ulPin = g_ADigitalPinMap[columns[i]]; 
-        if((pinreg>>ulPin)&1)  
-          {
-            uint8_t keycode = keymap[0*MATRIX_COLS + i];
-            activeKeys.push_back(keycode);
-            Serial.print("row: 0 Column");
-            Serial.println(i);
-            has_key = true;
-          }
-    }
-    // READ SECOND ROW
-    pinreg = (NRF_GPIO->IN)^0xffffffff;  // press is active high regardless of diode dir
-    pinMode(rows[1], INPUT); 
-// THIRD ROW
-    pinMode(rows[2], OUTPUT);
-    digitalWrite(rows[2],LOW);
-    // PROCESS SECOND ROW - need for the GPIO lines to settle down electrically before reading second row;
-    for (int i = 0; i < MATRIX_COLS; ++i){
-        int ulPin = g_ADigitalPinMap[columns[i]]; 
-        if((pinreg>>ulPin)&1)  
-          {
-            uint8_t keycode = keymap[1*MATRIX_COLS + i];
-            activeKeys.push_back(keycode);
-            Serial.print("row: 1 Column");
-            Serial.println(i);
-            has_key = true;
-          }
-    }
-    // READ THIRD ROW
-    pinreg = (NRF_GPIO->IN)^0xffffffff;  // press is active high regardless of diode dir
-    pinMode(rows[2], INPUT); 
-// FOURTH ROW
-    pinMode(rows[3], OUTPUT);
-    digitalWrite(rows[3],LOW);
-    // PROCESS THIRD ROW - need for the GPIO lines to settle down electrically before reading second row;
-    for (int i = 0; i < MATRIX_COLS; ++i){
-        int ulPin = g_ADigitalPinMap[columns[i]]; 
-        if((pinreg>>ulPin)&1)  
-          {
-            uint8_t keycode = keymap[2*MATRIX_COLS + i];
-            activeKeys.push_back(keycode); 
-            Serial.print("row: 2 Column");
-            Serial.println(i);
-            has_key = true;
-          }
-    }
-    // READ FOURTH ROW
-    pinreg = (NRF_GPIO->IN)^0xffffffff;  // press is active high regardless of diode dir
-    pinMode(rows[3], INPUT); 
-    // PROCESS FOURTH ROW - Process 4th row right away...
-    for (int i = 0; i < MATRIX_COLS; ++i){
-        int ulPin = g_ADigitalPinMap[columns[i]]; 
-        if((pinreg>>ulPin)&1)  
-          {
-            uint8_t keycode = keymap[3*MATRIX_COLS + i];
-            activeKeys.push_back(keycode);
-            Serial.print("row: 3 Column");
-            Serial.println(i);
-            has_key = true;
-          }
-    }
 
+    /*************/
+
+     for (int j = 1; j < MATRIX_ROWS; ++j){       
+      // NEXT ROW
+        pinMode(rows[j], OUTPUT);
+        digitalWrite(rows[j],LOW);
+        // PROCESS PREVIOUS ROW - need for the GPIO lines to settle down electrically before reading NEXT row;
+        for (int i = 0; i < MATRIX_COLS; ++i){
+            int ulPin = g_ADigitalPinMap[columns[i]]; 
+            if((pinreg>>ulPin)&1)  
+              {
+                uint16_t keycode = keymap[(j-1)*MATRIX_COLS + i];
+                activeKeys.push_back(keycode);
+                has_key = true;
+              }
+        }
+        // READ NEXT ROW
+        pinreg = (NRF_GPIO->IN)^0xffffffff;  // press is active high regardless of diode dir
+        pinMode(rows[j], INPUT); 
+     }
+
+    // PROCESS LAST ROW - Process 4th row right away...
+    for (int i = 0; i < MATRIX_COLS; ++i){
+        int ulPin = g_ADigitalPinMap[columns[i]]; 
+        if((pinreg>>ulPin)&1)  
+          {
+            uint16_t keycode = keymap[(MATRIX_ROWS-1)*MATRIX_COLS + i];
+            activeKeys.push_back(keycode);
+            has_key = true;
+          }
+    }
+    /*************/ 
     //Scanning done, disabling all columns - needed to save power
     for (int i = 0; i < MATRIX_COLS; ++i) {                             
         pinMode(columns[i], INPUT);                                     
     }
-    if ( activeKeys.empty() ) //&& has_key
+    return activeKeys;
+}
+
+trigger_keycodes_t sendKeys(trigger_keycodes_t activeKeys)
+{
+  static bool has_key = false;  // must be static to remember previous state
+  if ( activeKeys.empty() )
     {
       // send empty key report if previously has key pressed
       if (has_key) 
       { 
         bluemicro_hid.keyboardRelease();
-        Serial.println("Release"); 
+        has_key = false;
       }
-      has_key = false;
-      
     } 
     else
     {
+        has_key = true;
         uint8_t currentMod = 0;
         for (auto keycode : activeKeys) 
         {
@@ -155,8 +124,7 @@ void loop() {
             {
               // add hidKeycode to report vector
               reportvector.push_back(hidKeycode);
-            }
-            
+            }  
             //check if the hid keycode contains a modifier. // also check for macros.
             switch (hidKeycode) { 
                 case KC_LCTRL:  currentMod |= 1;    currentMod |= extraModifiers; break;
@@ -170,31 +138,30 @@ void loop() {
             }
             //add all of the extra modifiers into the curren modifier 
             currentMod |= extraModifiers;
-    
         }
 
-            uint8_t keycode[6] = { 0 };
-            uint8_t keycodeposition = 0;
-            for (auto thiskeycode : activeKeys) 
-            {
-              if (keycodeposition<6)
-              {
-                  keycode[keycodeposition] = thiskeycode;
-              }
-              keycodeposition++;
-            }
-            
-      
-            bluemicro_hid.keyboardReport(currentMod, keycode);
-            activeKeys.clear();
-            reportvector.clear();
-            Serial.println("Send");
+        uint8_t keycode[6] = { 0 };
+        uint8_t keycodeposition = 0;
+        for (auto thiskeycode : activeKeys) 
+        {
+          if (keycodeposition<6)
+          {
+              keycode[keycodeposition] = thiskeycode;
+          }
+          keycodeposition++;
+        }
+        bluemicro_hid.keyboardReport(currentMod, keycode);
+        activeKeys.clear();
+        reportvector.clear();
     }
+  return activeKeys;
+}
 
-    
+void loop() {
+  // put your main code here, to run repeatedly:  
+  activeKeys = scanMatrix(activeKeys);
+  activeKeys = sendKeys(activeKeys); 
   bluemicro_hid.processQueues(CONNECTION_MODE_AUTO);
-
-
-    // poll gpio once each 10 ms
+  // poll gpio once each 10 ms
   delay(10);
 }
